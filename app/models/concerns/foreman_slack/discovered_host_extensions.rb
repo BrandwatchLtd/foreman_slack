82e54be7586c501a7015cf3b8574e4377dae92c7
Luke Alexander <luke@brandwatch.com>
2017-10-04 15:20:03 +0100

    facts for discovered node to set location based message

--- app/models/concerns/foreman_slack/discovered_host_extensions.rb
+++ app/models/concerns/foreman_slack/discovered_host_extensions.rb
@@ -3,17 +3,23 @@ module ForemanSlack
     extend ActiveSupport::Concern
 
     included do
-      alias_method_chain :create_notification, :slack
+      alias_method_chain :import_facts, :slack
     end
 
     private
 
-    def create_notification_with_slack
+    def import_facts_with_slack(facts)
       if Setting[:notify_slack_discovered_host]
+        location = 'Unknown location'
+        if SETTINGS[:locations_enabled]
+          location = facts["foreman_location"] || 'Unknown location'
+          switch_chassis = facts["lldp_neighbor_sysname_eth0"] || 'Unknown'
+        end
         slack = ForemanSlack::SlackNotify.new
-        slack.notify(_('%s has been discovered!') % name)
+        slack.notify(_('New host: %s discovered in switch %s in %s') % [name,switch_chassis,location])
+        Rails.logger.info "ForemanSlack notification sent for #{name} attached to #{switch_chassis} in #{location}"
       end
-      create_notification_without_slack
+      import_facts_without_slack(facts)
     end
   end
 end
